6914eabfcf4799302891f4c50e816cd5e4131ca8
岡本斗志貴 <>
2024-08-06 19:45:32 +0900

    no message

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,7 +5,6 @@ void ofApp::setup(){
 
     ofSetFrameRate(60);
     ofSetVerticalSync(true);
-    //ofSetWindowShape(ofGetScreenWidth() * (16 / 18.), ofGetScreenWidth() / 2.);
     ofSetWindowShape(1920, 1080);
     ofSetWindowPosition((ofGetScreenWidth() - ofGetWidth()) / 2., (ofGetScreenHeight() - ofGetHeight()) / 2.);
     ofBackground(0);
@@ -16,25 +15,17 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
 
-    ofSetWindowTitle("Kumazawa Design | FPS " + ofToString(round(ofGetFrameRate())));
+    ofSetWindowTitle("Kumazawa Design 05 | FPS " + ofToString(round(ofGetFrameRate())));
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
     
     ofRectangle frame = ofGetWindowRect();
     
-    previousTime = time;
-    time = ofGetElapsedTimef();
-    
-    if (ofWrap(time, 0, 10) < ofWrap(previousTime, 0, 10)) index = ofWrap(index + 1, 0, 6);
+    float time = ofGetElapsedTimef();
 
-    if (index == 0) myVisual01.draw(frame, time);
-    else if (index == 1) myVisual02.draw(frame, time);
-    else if (index == 2) myVisual03.draw(frame, time);
-    else if (index == 3) myVisual04.draw(frame, time);
-    else if (index == 4) myVisual05.draw(frame, time);
-    else if (index == 5) myVisual06.draw(frame, time);
+    myVisual05.draw(frame, time);
 }
 
 //--------------------------------------------------------------
@@ -45,8 +36,6 @@ void ofApp::exit(){
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 
-    if (key == OF_KEY_RIGHT) index = ofWrap(index + 1, 0, 6);
-    if (key == OF_KEY_LEFT) index = ofWrap(index - 1, 0, 6);
 }
 
 //--------------------------------------------------------------
